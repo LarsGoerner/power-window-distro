a06e9ac36f41e0a6e005629cf2167b7a94f04cd6
Lars Görner <lgoerner@hotmail.de>
2026-07-14 10:07:50 +0200

    added brightness to settings

--- meta-power-window/recipes-apps/power-window-app/files/src/Backlight.cpp
+++ meta-power-window/recipes-apps/power-window-app/files/src/Backlight.cpp
@@ -2,14 +2,24 @@
 #include <QString>
 #include <QDir>
 #include <QFile>
+#include <QSettings>
 #include "Backlight.hpp"
+
+#define SET_PATH        "Backlight/"
+#define SET_BRT_PATH    SET_PATH "brightness"
 
 Backlight::Backlight(QObject * parent) : QObject(parent), mPath("/sys/class/backlight/waveshare_bl/")
 {
         QFile maxFile(mPath + "/max_brightness");
         if (maxFile.open(QIODevice::ReadOnly)) {
                 mMaxBrightness = maxFile.readAll().trimmed().toInt();
+        } else {
+                mMaxBrightness = 255; // just guessing the max value
         }
+        QSettings settings;
+        int savedRawBrightness = settings.value(SET_BRT_PATH, mMaxBrightness).toInt();
+        writeSysfs("brightness", savedRawBrightness);
+        emit brightnessChanged();
 }
 
 int Backlight::brightness() const
@@ -25,6 +35,8 @@ void Backlight::setBrightness(int perc)
         if (mPath.isEmpty() || mMaxBrightness <= 0) { return; }
         int raw = qBound(0, perc * mMaxBrightness / 100, mMaxBrightness);
         writeSysfs("brightness", raw);
+        QSettings settings;
+        settings.setValue(SET_BRT_PATH, raw);
         emit brightnessChanged();
 }
 
